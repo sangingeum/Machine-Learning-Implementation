598541a0a477979359a56505c85172490eb3b3dd
sangingeum <56298463+sangingeum@users.noreply.github.com>
2023-03-24 21:55:43 +0900

    Update train_loop function
    Changed names to make them easy to understand. average_train_loss variable is created to track the average train loss.

--- misc/utils.py
+++ misc/utils.py
@@ -14,6 +14,7 @@ def train_loop(X: torch.tensor, y: torch.tensor, epochs, test_ratio, model, devi
     test_data_loader = DataLoader(test_data_set, batch_size=batch_size, shuffle=True)
 
     for epoch in range(epochs):
+        average_train_loss = 0
         for i, train_data in enumerate(train_data_loader):
             X, y = train_data
             X = X.to(device)
@@ -21,7 +22,9 @@ def train_loop(X: torch.tensor, y: torch.tensor, epochs, test_ratio, model, devi
 
             model.train()
             y_prediction = model(X)
-            loss = loss_function(y, y_prediction)
+
+            loss = loss_function(y_prediction, y)
+            average_train_loss += loss
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -33,15 +36,20 @@ def train_loop(X: torch.tensor, y: torch.tensor, epochs, test_ratio, model, devi
             with torch.inference_mode():
                 average_test_loss = 0
                 for i, test_data in enumerate(test_data_loader):
-                    X, y = test_data
-                    X = X.to(device)
-                    y = y.to(device)
-                    test_y_prediction = model(X)
-                    test_loss = loss_function(y, test_y_prediction)
-                    average_test_loss += test_loss / batch_size
-                average_test_loss /= (i+1)
-                print("\ntrain loss: {}"
-                      "\ntest loss: {}".format(loss / batch_size, average_test_loss))
+                    test_X, test_y = test_data
+                    test_X = test_X.to(device)
+                    test_y = test_y.to(device)
+                    test_y_prediction = model(test_X)
+                    test_loss = loss_function(test_y_prediction, test_y)
+                    average_test_loss += test_loss
+                average_test_loss /= len(test_data_loader.dataset)
+                average_train_loss /= len(train_data_loader.dataset)
+                print("\nepoch: {}"
+                      "\ntrain loss: {}"
+                      "\ntest loss: {}".format(epoch, average_train_loss, average_test_loss))
+
+
+
 
 def get_device_name_agnostic():
     return "cuda" if torch.cuda.is_available() else "cpu"
