940060cddff30bb7018139de10757ab73f9aa392
sangingeum <56298463+sangingeum@users.noreply.github.com>
2023-03-24 23:02:41 +0900

    small changes before merging
    small changes such as gitignore changes

--- neural_network_regression_main.py
+++ neural_network_regression_main.py
@@ -1,9 +1,6 @@
-import torch
 from supervised.neural_network_regression import *
 from sklearn.datasets import load_diabetes
 from misc.utils import *
-
-
 
 def main():
     # device agnostic code
@@ -12,14 +9,14 @@ def main():
     X, y = load_diabetes(return_X_y=True, as_frame=False)
     y = y.reshape((-1, 1))
     # convert to torch.tensor
-    X = torch.from_numpy(X).to(torch.float32)
-    y = torch.from_numpy(y).to(torch.float32)
+    X = torch.from_numpy(X).type(torch.float32)
+    y = torch.from_numpy(y).type(torch.float32)
     # model, hyper parameters
     model = neural_network_regression(units_per_layer=[10, 16, 32, 16, 1]).to(device)
     loss_function = torch.nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    epochs = 10001
-    print_interval = 1000
+    epochs = 2001
+    print_interval = 100
     batch_size = 256
     test_ratio = 0.2
     #train loop
