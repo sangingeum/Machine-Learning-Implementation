aff71f5727c4615fcb3da3b232355b0aacb141fe
sangingeum <56298463+sangingeum@users.noreply.github.com>
2023-03-25 18:10:50 +0900

    Create neural_network_multi_label_classification.py
    Drop out layers are included.

---
+++ supervised/neural_network_multi_label_classification.py
@@ -0,0 +1,24 @@
+import torch
+from torch import nn
+
+class neural_network_multi_label_classification(nn.Module):
+    def __init__(self, units_per_layer=[2, 5, 1]):
+        super().__init__()
+        self.dropout_02 = nn.Dropout(p=0.2)
+        if len(units_per_layer) < 2:
+            raise Exception("Units_per_layer should be longer than 2.")
+        layers = []
+        for i in range(len(units_per_layer)-1):
+            layers.append(nn.Linear(units_per_layer[i], units_per_layer[i + 1]))
+            if i < len(units_per_layer) - 2:
+                layers.append(nn.ReLU())
+                layers.append(self.dropout_02)
+        layers.append(nn.Sigmoid())
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+
+
