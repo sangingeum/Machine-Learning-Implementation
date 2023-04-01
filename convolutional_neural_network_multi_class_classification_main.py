246bd2633308338ba221153dc2a7babc6b05ca35
sangingeum <56298463+sangingeum@users.noreply.github.com>
2023-04-01 20:21:21 +0900

    Separated train_transformations and test_transformations
    SSeparated train_transformations and test_transformations

--- convolutional_neural_network_multi_class_classification_main.py
+++ convolutional_neural_network_multi_class_classification_main.py
@@ -1,21 +1,24 @@
 from supervised.convolutional_neural_network_multi_class_classification import *
 from torchvision.datasets import CIFAR10
 from torchvision import transforms
-from torchvision.transforms import ToTensor
-from data_preprocessing.normalization import *
 from misc.utils import *
 
 def main():
     # device agnostic code
     device = get_device_name_agnostic()
-    # transforamtions
-    transforamtions = transforms.Compose([
-        ToTensor(),
-        Normalize(0.5, 0.5)
+    # transformations
+    train_transformations = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(0.5, 0.5)
+    ])
+    test_transformations = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(0.5, 0.5)
     ])
     # load data
-    train_data_set = CIFAR10(root="data", train=True, download=True, transform=transforamtions)
-    test_data_set = CIFAR10(root="data", train=False, download=True, transform=transforamtions)
+    train_data_set = CIFAR10(root="data", train=True, download=True, transform=train_transformations)
+    test_data_set = CIFAR10(root="data", train=False, download=True, transform=test_transformations)
 
     # model, hyper parameters
     model = convolutional_neural_network_multi_class_classification(32, 32, channels=[3, 32, 128],
@@ -27,7 +30,7 @@ def main():
     print_interval = 1
     batch_size = 1024
 
-    #train loop
+    # train loop
     train_loop(train_data_set=train_data_set, test_data_set=test_data_set, epochs=epochs, model=model, device=device,
                batch_size=batch_size, loss_function=loss_function, optimizer=optimizer, print_interval=print_interval,
                weighted_sample=False, accuracy_function=calculate_accuracy_multi_class)
