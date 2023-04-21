81a56f496641ad0b9090a284c3be91019e491f16
sangingeum <56298463+sangingeum@users.noreply.github.com>
2023-04-22 01:50:15 +0900

    Enforce consistent naming
    Enforce consistent naming

--- reinforcement_policy_iteration_main.py
+++ reinforcement_policy_iteration_main.py
@@ -5,8 +5,8 @@ from reinforcement.reinforcement_policy_iteration import *
 def main():
     env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=True)
     slippery_MDP = create_FrozenLake_v1_4x4_slippery_MDP()
-    value_iteration = reinforcement_policy_iteration(env.observation_space.n, env.action_space.n, slippery_MDP, theta=0.001)
-    value_iteration.fit()
+    model = reinforcement_policy_iteration(env.observation_space.n, env.action_space.n, slippery_MDP, theta=0.001)
+    model.fit()
 
     episodes = 100
     return_sum = 0
@@ -14,7 +14,7 @@ def main():
         state = env.reset()[0]
         ret = 0
         while True:
-            action = value_iteration.greedy_policy(state)
+            action = model.greedy_policy(state)
             next_state, reward, done, _, _ = env.step(action)
             ret += reward
             state = next_state
@@ -25,9 +25,9 @@ def main():
     print()
     print("Average return: {}".format(return_sum/episodes))
     print("Value")
-    print(value_iteration.value.reshape((4, 4)))
+    print(model.value.reshape((4, 4)))
     print("Policy")
-    print(value_iteration.policy.reshape((4, 4)))
+    print(model.policy.reshape((4, 4)))
 
 
 
