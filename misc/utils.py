62a0e1f8d20e2153a22776f81ab5f3ecfc1e0755
sangingeum <56298463+sangingeum@users.noreply.github.com>
2023-04-23 03:17:29 +0900

    Add parameter drop last, collate_fn, X_on_the_fly_function
    Add parameter drop last, collate_fn, X_on_the_fly_function

--- misc/utils.py
+++ misc/utils.py
@@ -9,12 +9,14 @@ import numpy as np
 import torch.utils.data as data
 
 
-def calculate_test_loss(model, device, loss_function, test_data_loader):
+def calculate_test_loss(model, device, loss_function, test_data_loader, X_on_the_fly_function=None):
     model.eval()
     with torch.inference_mode():
         average_test_loss = 0
         for test_data in test_data_loader:
             test_X, test_y = test_data
+            if X_on_the_fly_function is not None:
+                test_X = X_on_the_fly_function(test_X)
             test_X = test_X.to(device)
             test_y = test_y.to(device)
             test_y_prediction = model(test_X)
@@ -33,13 +35,15 @@ def calculate_correct(y_pred, y_true):
         raise Exception(f"Shape mismatch: y_pred shape is {y_pred.shape} but y_true shape is {y_true.shape}")
     return torch.sum(torch.all(torch.eq(y_pred, y_true), dim=1))
 
-def calculate_accuracy_one_hot(model, test_data_loader, device):
+def calculate_accuracy_one_hot(model, test_data_loader, device, X_on_the_fly_function=None):
     model.eval()
     correct = 0
     total = 0
     with torch.inference_mode():
         for data in test_data_loader:
             X, y = data
+            if X_on_the_fly_function is not None:
+                X = X_on_the_fly_function(X)
             X = X.to(device)
             y = y.to(device)
             outputs = model(X)
@@ -49,13 +53,15 @@ def calculate_accuracy_one_hot(model, test_data_loader, device):
     return 100 * correct / total
 
 
-def calculate_accuracy_multi_class(model, test_data_loader, device):
+def calculate_accuracy_multi_class(model, test_data_loader, device, X_on_the_fly_function=None):
     model.eval()
     correct = 0
     total = 0
     with torch.inference_mode():
         for data in test_data_loader:
             X, y = data
+            if X_on_the_fly_function is not None:
+                X = X_on_the_fly_function(X)
             X = X.to(device)
             y = y.to(device)
             outputs = model(X)
@@ -64,13 +70,15 @@ def calculate_accuracy_multi_class(model, test_data_loader, device):
             total += len(y)
     return 100*correct/total
 
-def calculate_accuracy_binary_class(model, test_data_loader, device):
+def calculate_accuracy_binary_class(model, test_data_loader, device, X_on_the_fly_function=None):
     model.eval()
     correct = 0
     total = 0
     with torch.inference_mode():
         for data in test_data_loader:
             X, y = data
+            if X_on_the_fly_function is not None:
+                X = X_on_the_fly_function(X)
             X = X.to(device)
             y = y.to(device)
             outputs = model(X)
@@ -80,13 +88,15 @@ def calculate_accuracy_binary_class(model, test_data_loader, device):
     return 100 * correct / total
 
 
-def calculate_accuracy_binary_class_with_sigmoid(model, test_data_loader, device):
+def calculate_accuracy_binary_class_with_sigmoid(model, test_data_loader, device, X_on_the_fly_function=None):
     model.eval()
     correct = 0
     total = 0
     with torch.inference_mode():
         for data in test_data_loader:
             X, y = data
+            if X_on_the_fly_function is not None:
+                X = X_on_the_fly_function(X)
             X = X.to(device)
             y = y.to(device)
             outputs = model(X)
@@ -104,8 +114,10 @@ def print_learning_progress(epoch, train_loss, test_loss, accuracy=None):
         progress_string += "\naccuracy: {}".format(accuracy)
     print(progress_string)
 
+
 def train_loop(train_data_set, test_data_set, epochs, model, device, batch_size, loss_function, optimizer,
-               print_interval, weighted_sample=False, accuracy_function=None):
+               print_interval, weighted_sample=False, accuracy_function=None, X_on_the_fly_function=None,
+               collate_fn=torch.utils.data.default_collate, test_first=False):
 
     # create data loader
     if weighted_sample:
@@ -116,16 +128,22 @@ def train_loop(train_data_set, test_data_set, epochs, model, device, batch_size,
         else:
             train_sampler = get_weighted_sampler(torch.tensor(train_data_set.targets))
 
-        train_data_loader = DataLoader(train_data_set, batch_size=batch_size, sampler=train_sampler)
-        test_data_loader = DataLoader(test_data_set, batch_size=batch_size, shuffle=True)
+        train_data_loader = DataLoader(train_data_set, batch_size=batch_size, sampler=train_sampler, collate_fn=collate_fn, drop_last=True)
+        test_data_loader = DataLoader(test_data_set, batch_size=batch_size, shuffle=True, collate_fn=collate_fn, drop_last=True)
     else:
-        train_data_loader = DataLoader(train_data_set, batch_size=batch_size, shuffle=True)
-        test_data_loader = DataLoader(test_data_set, batch_size=batch_size, shuffle=True)
+        train_data_loader = DataLoader(train_data_set, batch_size=batch_size, shuffle=True, collate_fn=collate_fn, drop_last=True)
+        test_data_loader = DataLoader(test_data_set, batch_size=batch_size, shuffle=True, collate_fn=collate_fn, drop_last=True)
 
-    for epoch in range(epochs):
+    if test_first:
+        print_progress(train_data_loader, test_data_loader, model, device, 0, loss_function, 0, accuracy_function, X_on_the_fly_function)
+
+    for epoch in range(1, epochs+1):
         average_train_loss = 0
         for train_data in train_data_loader:
+
             X, y = train_data
+            if X_on_the_fly_function is not None:
+                X = X_on_the_fly_function(X)
             X = X.to(device)
             y = y.to(device)
 
@@ -141,13 +159,17 @@ def train_loop(train_data_set, test_data_set, epochs, model, device, batch_size,
         if print_interval <= 0:
             continue
         if epoch % print_interval == 0:
-            average_train_loss /= len(train_data_loader.dataset)
-            average_test_loss = calculate_test_loss(model, device, loss_function, test_data_loader)
-            if accuracy_function is None:
-                print_learning_progress(epoch, average_train_loss, average_test_loss)
-            else:
-                accuracy = accuracy_function(model, test_data_loader, device)
-                print_learning_progress(epoch, average_train_loss, average_test_loss, accuracy)
+            print_progress(train_data_loader, test_data_loader, model, device, epoch, loss_function, average_train_loss, accuracy_function, X_on_the_fly_function)
+
+def print_progress(train_data_loader, test_data_loader, model, device, epoch, loss_function, average_train_loss, accuracy_function=None, X_on_the_fly_function=None):
+    average_train_loss /= len(train_data_loader.dataset)
+    average_test_loss = calculate_test_loss(model, device, loss_function, test_data_loader, X_on_the_fly_function)
+    if accuracy_function is None:
+        print_learning_progress(epoch, average_train_loss, average_test_loss)
+    else:
+        accuracy = accuracy_function(model, test_data_loader, device, X_on_the_fly_function)
+        print_learning_progress(epoch, average_train_loss, average_test_loss, accuracy)
+
 
 def print_class_distribution(y: numpy.array, is_one_hot=False):
     if is_one_hot:
