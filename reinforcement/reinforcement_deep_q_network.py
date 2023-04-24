47b6c355f23c61e1a8bbf16e21aef1999b679e81
sangingeum <56298463+sangingeum@users.noreply.github.com>
2023-04-25 01:15:56 +0900

    Implement n-step DQN
    Implement n-step DQN for solving the cartpole environment

--- reinforcement/reinforcement_deep_q_network.py
+++ reinforcement/reinforcement_deep_q_network.py
@@ -1,9 +1,6 @@
 from torch import nn
-from replay_buffer import *
+from reinforcement.replay_buffer import *
 from copy import deepcopy
-
-
-
 
 class reinforcement_deep_q_network:
     """
@@ -27,19 +24,21 @@ class reinforcement_deep_q_network:
         self.curr_step = 0
         self.learning_rate = 0.0003
         self.buffer_size = 50000
-        self.batch_size = 64
+        self.learning_start_buffer_size = 25000
+        self.batch_size = 128
         self.epsilon = 1.0
-        self.min_epsilon = 0.05
+        self.min_epsilon = 0.005
         self.epsilon_decay = 0.9999
         self.gamma = 0.85
-        self.n_step = 3
-        self.target_update_freq = 128
+        self.n_step = 1
         self.gradient_update_freq = 1
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.target_network = deepcopy(self.network)
         self.optimizer = torch.optim.Adam(params=self.network.parameters(), lr=self.learning_rate)
         self.loss_function = torch.nn.HuberLoss()
         self.replay_buffer = replay_buffer(buffer_size=self.buffer_size, n_step=self.n_step, gamma=self.gamma)
+
+        # device
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.network = self.network.to(self.device)
 
     def epsilon_greedy_policy(self, state):
         if np.random.uniform(0, 1) < self.epsilon:
@@ -56,15 +55,9 @@ class reinforcement_deep_q_network:
             return np.random.randint(self.action_size)
 
     def train_network(self, states, actions, rewards, next_states, dones):
+        target_Q_s = torch.max(self.network(next_states), dim=1)[0] * (~dones)
+        td_targets = (rewards + (self.gamma ** self.n_step) * target_Q_s).to(torch.float32)
         self.network.train()
-        self.target_network.train()
-        target_Q_s = self.target_network(next_states)
-        target_actions = torch.argmax(self.network(next_states), dim=1)
-
-        max_target_Q_s = target_Q_s[torch.arange(actions.size(0)), target_actions]
-        max_target_Q_s = max_target_Q_s * (~dones)
-        td_targets = (rewards + (self.gamma ** self.n_step) * max_target_Q_s).to(torch.float32)
-
         Q_s = self.network(states)
         Q_sa = Q_s[torch.arange(actions.size(0)), actions.type(torch.LongTensor)]
 
@@ -74,16 +67,12 @@ class reinforcement_deep_q_network:
         self.optimizer.step()
 
         self.epsilon = max(self.min_epsilon, self.epsilon * self.epsilon_decay)
-        if self.curr_step % self.target_update_freq == 0:
-            self.update_target_network()
-
-    def update_target_network(self):
-        self.target_network = deepcopy(self.network)
 
     def step(self, state, action, reward, next_state, done):
         self.curr_step += 1
         self.replay_buffer.add(state, action, reward, next_state, done)
-
-        if len(self.replay_buffer) > self.batch_size and self.curr_step % self.gradient_update_freq == 0:
+        if self.learning_start_buffer_size < self.curr_step and \
+                len(self.replay_buffer) > self.batch_size and \
+                self.curr_step % self.gradient_update_freq == 0:
             self.train_network(*self.replay_buffer.sample(self.batch_size, device=self.device))
 
---
+++ reinforcement_deep_q_network_main.py
@@ -0,0 +1,28 @@
+from reinforcement.reinforcement_deep_q_network import *
+import gymnasium as gym
+
+if __name__ == "__main__":
+    env = gym.make('CartPole-v1')
+    total_steps = 50000
+    cur_steps = 0
+    max_episodes = 10000
+    model = reinforcement_deep_q_network(units_per_layer=[4, 128, 64, 32, 2])
+    for episode in range(max_episodes):
+        state = env.reset()[0]
+        reward_sum = 0
+        while True:
+            action = model.epsilon_greedy_policy(state=state)
+            next_state, reward, done, _, _ = env.step(action)
+            reward_sum += reward
+            # reward shaping
+            reward -= abs(state[2])*2
+            # model step
+            model.step(state=state, action=action, reward=reward, next_state=next_state, done=done)
+            # update status
+            cur_steps += 1
+            state = next_state
+            if done:
+                print("episode: {}, reward sum: {}, steps used: {}, epsilon: {}".format(episode, reward_sum, cur_steps, model.epsilon))
+                break
+        if cur_steps >= total_steps:
+            break
