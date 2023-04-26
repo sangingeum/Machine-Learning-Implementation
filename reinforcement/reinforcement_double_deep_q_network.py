5a49291a44a53fe0f115e1f015736b15d69d0590
sangingeum <56298463+sangingeum@users.noreply.github.com>
2023-04-26 19:35:55 +0900

    DDQN with PER
    DDQN with PER

--- reinforcement/reinforcement_double_deep_q_network.py
+++ reinforcement/reinforcement_double_deep_q_network.py
@@ -1,12 +1,13 @@
 from torch import nn
 from reinforcement.replay_buffer import *
+from reinforcement.prioritized_replay_buffer import *
 from copy import deepcopy
 
 class reinforcement_double_deep_q_network:
     """
     n-step DDQN with a replay buffer and a target network
     """
-    def __init__(self, units_per_layer=[4, 64, 128, 64, 2]):
+    def __init__(self, units_per_layer=[4, 64, 128, 64, 2], use_PER=False):
         # model creation
         self.network = self._build_model(units_per_layer)
         # hyper parameter setting
@@ -26,7 +27,11 @@ class reinforcement_double_deep_q_network:
         self.gradient_update_freq = 1
         self.optimizer = torch.optim.Adam(params=self.network.parameters(), lr=self.learning_rate)
         self.loss_function = torch.nn.HuberLoss()
-        self.replay_buffer = replay_buffer(buffer_size=self.buffer_size, n_step=self.n_step, gamma=self.gamma)
+        self.use_PER = use_PER
+        if use_PER:
+            self.replay_buffer = prioritized_replay_buffer(buffer_size=self.buffer_size, n_step=self.n_step, gamma=self.gamma)
+        else:
+            self.replay_buffer = replay_buffer(buffer_size=self.buffer_size, n_step=self.n_step, gamma=self.gamma)
 
         # device
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -58,28 +63,39 @@ class reinforcement_double_deep_q_network:
         if np.random.uniform(0, 1) < self.epsilon:
             return np.random.randint(self.action_size)
 
-    def train_network(self, states, actions, rewards, next_states, dones):
-        self.network.train()
-        self.target_network.train()
-
+    def calculate_TD_targets(self, actions, rewards, next_states, dones):
         target_Q_s = self.target_network(next_states)
         target_actions = torch.argmax(self.network(next_states), dim=1)
-
         max_target_Q_s = target_Q_s[torch.arange(actions.size(0)), target_actions]
         max_target_Q_s = max_target_Q_s * (~dones)
         td_targets = (rewards + (self.gamma ** self.n_step) * max_target_Q_s).to(torch.float32)
+        return td_targets
 
-        Q_s = self.network(states)
-        Q_sa = Q_s[torch.arange(actions.size(0)), actions.type(torch.LongTensor)]
+    def calculate_Q_values(self, states, actions):
+        predicted_Q_values_for_all_actions = self.network(states)
+        predicted_Q_values = predicted_Q_values_for_all_actions[torch.arange(actions.size(0)), actions.type(torch.LongTensor)]
+        return predicted_Q_values
+
+    def train_network(self, states, actions, rewards, next_states, dones, weights=None):
+        self.network.train()
+        self.target_network.train()
+
+        td_targets = self.calculate_TD_targets(actions, rewards, next_states, dones)
+        predicted_Q_values = self.calculate_Q_values(states, actions)
 
         self.optimizer.zero_grad()
-        loss = self.loss_function(Q_sa, td_targets)
+        loss = self.loss_function(predicted_Q_values, td_targets)
+        if weights is not None:
+            loss = torch.mean(weights*loss)
         loss.backward()
         self.optimizer.step()
 
         self.epsilon = max(self.min_epsilon, self.epsilon * self.epsilon_decay)
         if self.curr_step % self.target_update_freq == 0:
             self.update_target_network()
+
+        if self.use_PER:
+            return torch.abs(predicted_Q_values - td_targets)
 
     def update_target_network(self):
         self.target_network = deepcopy(self.network)
@@ -90,5 +106,10 @@ class reinforcement_double_deep_q_network:
         if self.learning_start_buffer_size < self.curr_step and \
                 len(self.replay_buffer) > self.batch_size and \
                 self.curr_step % self.gradient_update_freq == 0:
-            self.train_network(*self.replay_buffer.sample(self.batch_size, device=self.device))
+            if self.use_PER:
+                states, actions, rewards, next_states, dones, indices, weights = self.replay_buffer.sample(self.batch_size, device=self.device)
+                td_errors = self.train_network(states, actions, rewards, next_states, dones, weights)
+                self.replay_buffer.update_TD_errors(indices, td_errors.detach().cpu())
+            else:
+                self.train_network(*self.replay_buffer.sample(self.batch_size, device=self.device))
 
