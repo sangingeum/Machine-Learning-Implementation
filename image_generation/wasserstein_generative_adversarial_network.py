0c916c131066effa52ae2f09fb9dc463c7de8127
sangingeum <56298463+sangingeum@users.noreply.github.com>
2023-05-05 04:02:55 +0900

    Delete GAN & Implement WGAN
    Delete GAN & Implement WGAN

--- image_generation/wasserstein_generative_adversarial_network.py
+++ image_generation/wasserstein_generative_adversarial_network.py
@@ -1,7 +1,7 @@
 from torch import nn
 import numpy as np
 
-class GANGenerator(nn.Module):
+class WGANGenerator(nn.Module):
     def __init__(self, img_shape, noise_size, units_per_layer=[128, 256, 512, 1024]):
         super().__init__()
         self.img_shape = img_shape
@@ -14,18 +14,19 @@ class GANGenerator(nn.Module):
         flattened_img = self.model(z)
         img = flattened_img.view((z.shape[0], *self.img_shape))
         return img
+
     def _build_model(self, units_per_layer):
         layers = []
         for i in range(len(units_per_layer) - 1):
-            layers.append(nn.Linear(units_per_layer[i], units_per_layer[i+1]))
-            layers.append(nn.BatchNorm1d(num_features=units_per_layer[i+1]))
-            layers.append(nn.LeakyReLU(negative_slope=0.2, inplace=True))
+            layers.append(nn.Linear(units_per_layer[i], units_per_layer[i + 1]))
+            layers.append(nn.BatchNorm1d(num_features=units_per_layer[i + 1]))
+            layers.append(nn.LeakyReLU(negative_slope=0.2))
         layers = layers[:-2]
         layers.append(nn.Tanh())
         return nn.Sequential(*layers)
 
 
-class GANDiscriminator(nn.Module):
+class WGANDiscriminator(nn.Module):
     def __init__(self, img_shape, units_per_layer=[512, 256, 128]):
         super().__init__()
         self.img_shape = img_shape
@@ -41,7 +42,7 @@ class GANDiscriminator(nn.Module):
         layers = []
         for i in range(len(units_per_layer) - 1):
             layers.append(nn.Linear(units_per_layer[i], units_per_layer[i+1]))
+            layers.append(nn.Dropout(p=0.2))
             layers.append(nn.LeakyReLU(negative_slope=0.2, inplace=True))
         layers = layers[:-1]
-        layers.append(nn.Softmax(dim=1))
         return nn.Sequential(*layers)
---
+++ wasserstein_generative_adversarial_network_MNIST_main.py
@@ -0,0 +1,75 @@
+import torch
+from torchvision.datasets import MNIST
+from image_generation.wasserstein_generative_adversarial_network import *
+from misc.utils import *
+from torchvision import transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == "__main__":
+    # define transformations
+    train_transformations = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=0.5, std=0.5)])
+    test_transformations = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=0.5, std=0.5)])
+    # Load datasets
+    train_data_set = MNIST(root="data", train=True, download=True, transform=train_transformations)
+    test_data_set = MNIST(root="data", train=False, download=True, transform=test_transformations)
+    epochs = 500
+    loss_print_interval = 1
+    image_print_interval = 25
+    batch_size = 9192
+    latent_vector_size = 2
+    lr = 0.0001
+    # Load models
+    device = get_device_name_agnostic()
+    gen_model = WGANGenerator(img_shape=(28, 28), noise_size=latent_vector_size, units_per_layer=[128, 256, 512, 1024]).to(device)
+    dsc_model = WGANDiscriminator(img_shape=(28, 28), units_per_layer=[512, 256, 128]).to(device)
+    # Set optimizers and hyper parameters
+    loss_function = torch.nn.BCELoss()
+    g_optimizer = torch.optim.RMSprop(gen_model.parameters(), lr=lr)
+    d_optimizer = torch.optim.RMSprop(dsc_model.parameters(), lr=lr)
+    # define data loaders
+    train_data_loader = DataLoader(train_data_set, batch_size=batch_size, shuffle=True, drop_last=True)
+    test_data_loader = DataLoader(test_data_set, batch_size=batch_size, shuffle=True, drop_last=True)
+    # train loop
+    valid = torch.ones((batch_size, 1), requires_grad=False).to(device)
+    fake = torch.zeros((batch_size, 1), requires_grad=False).to(device)
+
+    for epoch in range(1, epochs+1):
+        for i, train_data in enumerate(train_data_loader):
+            gen_model.train()
+            dsc_model.train()
+            valid_images, _ = train_data
+            valid_images = valid_images.to(device)
+
+            # train dsc_model
+            z = torch.from_numpy(np.random.standard_normal((batch_size, latent_vector_size))).type(torch.float32).to(device)
+            gen_images = gen_model(z).detach()
+            d_loss = torch.mean(dsc_model(gen_images)) - torch.mean(dsc_model(valid_images))
+            d_optimizer.zero_grad()
+            d_loss.backward()
+            d_optimizer.step()
+
+            # clamp dsc_model weights
+            for p in dsc_model.parameters():
+                p.data.clamp_(-0.01, 0.01)
+
+            if i % 5 == 0:
+                # train gen_model
+                gen_images = gen_model(z)
+                g_loss = -torch.mean(dsc_model(gen_images))
+                g_optimizer.zero_grad()
+                g_loss.backward()
+                g_optimizer.step()
+
+        if epoch % loss_print_interval == 0:
+            print("epoch: {}, g loss:{}, d loss: {}".format(epoch, g_loss.item(), d_loss.item()))
+        if epoch % image_print_interval == 0:
+            gen_model.eval()
+            with torch.inference_mode():
+                z = torch.from_numpy(np.random.standard_normal((25, latent_vector_size))).type(torch.float32).to(device)
+                gen_images = gen_model(z)
+            fig, axes = plt.subplots(nrows=5, ncols=5, figsize=(36, 36))
+            for i, ax in enumerate(axes.flat):
+                ax.imshow(gen_images[i].detach().cpu().numpy())
+                ax.axis('off')
+            plt.show()
